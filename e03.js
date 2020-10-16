39745a3c7eac0e6c56cc6fa8adaaeb9e7c05e0b6
Timberzzman <leloupandco@gmail.com>
2020-10-16 11:26:50 +0200

    Adding e03

---
+++ e03.js
@@ -0,0 +1,20 @@
+function marsos(str) {
+    let SOS = "SOS"
+    let result = 0
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == SOS[0]) {
+            for (let j = 0; j <= SOS.length && (j + i) < str.length; j++) {
+                if (j == SOS.length) {
+                    i+= j - 1
+                    break
+                }
+                else if (str[i + j] != SOS[j]) {
+                    result++
+                    i+= j
+                    break
+                }
+            }
+        }
+    }
+    return result
+}
