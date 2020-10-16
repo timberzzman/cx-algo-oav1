2143cc47123d69bac5eae5992fe4f7ee14ecc4b3
Timberzzman <leloupandco@gmail.com>
2020-10-16 10:26:30 +0200

    Adding e01

---
+++ e01.js
@@ -0,0 +1,9 @@
+function HowManyCamelCase(str) {
+    let result = 1
+    for (let i = 0; i < str.length; i++)
+    {
+        let code = str.charCodeAt(i)
+        if (code >= 65 && code <= 90) result++
+    }
+    return result;
+}
